3db693866b75a99689cc8e232d102dc967445275
greengraca <jopegraca@gmail.com>
2026-04-15 03:32:38 +0100

    Bump LATEST_EXT_VERSION to 1.5.5

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const DB_NAME = process.env.MONGODB_DB_NAME || "misstep";
 export const COLLECTION_PREFIX = "dashboard_";
 
 // Extension version check
-export const LATEST_EXT_VERSION = "1.5.4";
+export const LATEST_EXT_VERSION = "1.5.5";
 export const EXT_DOWNLOAD_URL = process.env.EXT_DOWNLOAD_URL || "";
